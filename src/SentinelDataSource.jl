77b38fdb8cf882fff624742c4757a856f3eb9674
Felix Cremer <fcremer@bgc-jena.mpg.de>
2025-11-07 16:25:49 +0100

    Fix loading of the data so that we get a functioning DimArray at the leaves

--- src/SentinelDataSource.jl
+++ src/SentinelDataSource.jl
@@ -1,23 +1,29 @@
 module SentinelDataSource
-using DimensionalData: DimTree
+using DimensionalData: DimTree, DimArray
 using Zarr: zopen
 using ZarrDatasets: ZarrDataset
 using CommonDataModel: CommonDataModel as CDM
-using Rasters:Raster
+using Rasters: Rasters
 
 export open_tree
 
 open_tree(path::AbstractString) = open_tree(ZarrDataset(path))
 
 function open_tree(dataset::ZarrDataset)
-    stem = DimTree()
-    groupnames = CDM.groupnames(dataset)
-    varnames = CDM.varnames(dataset)
-    alldimnames = nesteddimnames(dataset)
-    for v in setdiff(varnames, alldimnames)
-        setindex!(stem, Raster(CDM.variable(dataset, v), lazy=true),Symbol(v))
+    @time "stem" stem = DimTree()
+    @time "groups" groupnames = CDM.groupnames(dataset)
+    @time "vars" varnames = CDM.varnames(dataset)
+    @time "dims" alldimnames = nesteddimnames(dataset)
+    @time "forvar" for v in setdiff(varnames, alldimnames)
+        @time "var" var = CDM.variable(dataset, v)
+        @time "vardims" vardims = Rasters._dims(var)
+        @time "meta" metadata_out = Rasters._metadata(var)
+        @time "missing" missingval_out = Rasters._read_missingval_pair(var, metadata_out, Rasters.nokw)
+        @time "mod" mod = Rasters._mod(eltype(var), metadata_out, missingval_out;scaled=true, coerce=true)
+        @time "vardata" vardata = Rasters._maybe_modify(var, mod)
+        @time "set" setindex!(stem, DimArray(vardata, vardims),Symbol(v))
     end
-    for g in groupnames
+    @time "forg" for g in groupnames
         setindex!(stem,  open_tree(CDM.group(dataset, g)),Symbol(g))
     end
     stem
